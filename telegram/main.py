2292738bde11918728f70d846541f0c653bdea39
Sergei Sidorov <s.sidorov1990@mail.ru>
2025-12-10 15:50:18 +0300

    Add: Added README.md a file with a description of running scripts
    Changes to be committed:
    	new file:   .env
    	new file:   README.md
    	new file:   architectural_challenge/README.md
    	modified:   email_domen/main.py
    	new file:   requirements.txt
    	modified:   telegram/main.py

--- telegram/main.py
+++ telegram/main.py
@@ -2,11 +2,12 @@ import os
 import requests
 from dotenv import load_dotenv
 
+
 def main():
     load_dotenv()
 
     BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-    FILE_PATH = "text.txt"  
+    FILE_PATH = "text.txt"
 
     with open(FILE_PATH, "r", encoding="utf-8") as f:
         text = f.read()
@@ -14,10 +15,7 @@ def main():
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
     chat_id = input()
-    payload = {
-        "chat_id": chat_id,
-        "text": text
-    }
+    payload = {"chat_id": chat_id, "text": text}
 
     r = requests.post(url, json=payload)
 
@@ -26,5 +24,6 @@ def main():
     else:
         print("Ошибка:", r.text)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
